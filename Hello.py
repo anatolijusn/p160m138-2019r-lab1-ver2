e3eb4cdd2a1d27da7000b75d8ed88fbdc26f6fbc
anatolijus.nachumovas <anatolijus.nachumovas@ktu.edu>
2019-10-30 18:51:52 +0200

    feature-variable-shout condition

--- Hello.py
+++ Hello.py
@@ -1,11 +1,17 @@
 import sys
 
+def greet(name: str, shout_count: int = 1):
 
-
-def greet(name: str):
- return f"Hello, {name}!"
+    if shout_count is greet.__defaults__[0]:
+       str=f"Hello, {name}{shout_count * '!'}"
+       return str    
+    else:
+      return f"Hello, {name}{shout_count * '!'}"
 def testgreet():
     name=sys.argv[1]
-    print(greet(name))
-
+    if len(sys.argv)>2:
+       shout_count=int(sys.argv[2])
+       print(greet(name,shout_count))
+    else:
+      print(greet(name))
 testgreet()
